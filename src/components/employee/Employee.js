1062edad41dd55e5676edbd47a86db67cdedf695
tgann11 <morganne1170@gmail.com>
2021-09-28 09:11:15 -0500

    created a static version of the app and added the API

---
+++ src/components/employee/Employee.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "./Employee.css"
+
+export const EmployeeCard = () => (
+    <section className="employee">
+        <h3 className="employee__name">Moon Unit</h3>
+        <div className="employee__address">Nashville Kennels North</div>
+    </section>
+)
