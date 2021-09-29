d22269b314828f19db9ce9e2d9bd0159ba61c214
tgann11 <morganne1170@gmail.com>
2021-09-29 09:54:05 -0500

    added date component to home page

--- src/components/PropsAndState.js
+++ src/components/PropsAndState.js
@@ -8,11 +8,17 @@ export const PropsAndState = ({ yourName }) => {
         setCountClicks(newCountClicks)
     }
 
+    const current = new Date();
+    const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
+
+
     return (
         <>
             <h3>Welcome, {yourName} </h3>
+            <p>Today's Date {date}, remember to drink water</p>
             <p>{countClicks}</p>
             <button onClick={(handleClick)}>Click Me</button>
         </>
     )
 }
+
